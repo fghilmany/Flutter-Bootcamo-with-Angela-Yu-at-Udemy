823beaaebcbb43e7f0575014a46d3c467ec12e5f
Faris Ghilmany <fghilmany62@gmail.com>
2023-08-25 05:09:42 +0700

    initial commit

---
+++ i_am_poor/android/app/src/main/kotlin/com/fghilmany/i_am_poor/i_am_poor/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.fghilmany.i_am_poor.i_am_poor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ i_am_rich/android/app/src/main/kotlin/com/fghillmany/i_am_rich/i_am_rich/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.fghillmany.i_am_rich.i_am_rich
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
